8382707dff361e4a6f51d292a3980949d06b85fd
Anxo Perez Vila <anxo.pvila@udc.es>
2025-04-02 10:25:13 +0200

    Update with llama3.1:70b results

--- src/llms/structured_output/singularity/singularity_run.sh
+++ src/llms/structured_output/singularity/singularity_run.sh
@@ -7,11 +7,13 @@
 #SBATCH --mem-per-cpu=16G
 #SBATCH -o /mnt/experiments/slurm/logs/anxo/%x-%j.out # File to which STDOUT will be written
 #SBATCH -e /mnt/experiments/slurm/logs/anxo/%x-%j.err # File to which STDERR will be written
-################################################ tulkas -> namo -> orome -> elrond -> elros ###############################################
+################################################ tulkas -> orome -> namo (link) -> elrond -> elros ###############################################
 
 
 LLM_MODEL=$1
 export HF_HOME="/mnt/gpu-fastdata/hf-cache/hub" 
+export OLLAMA_MODELS="/mnt/gpu-fastdata/ollama-cache"
+export OLLAMA_LOAD_TIMEOUT="15m"
 
 
 SIF="/mnt/experiments/slurm/singularity-containers/anxo/ollama-server.sif"
